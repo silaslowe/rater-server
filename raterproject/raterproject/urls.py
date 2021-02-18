e7f72b9d7b440b607a371fe6fb40282c79a51fd7
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-02-18 10:18:14 -0600

    games back werks in postman

--- raterproject/raterproject/urls.py
+++ raterproject/raterproject/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import routers
 from django.conf.urls import include
 from django.urls import path
 from raterapi.views import register_user, login_user
+from raterapi.views import Games
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'games', Games, 'game')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
